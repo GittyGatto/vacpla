38ac0652b77c99607c5d427b88ebf5aef7cb0e97
GittyGatto <rhenning80@gmx.de>
2021-06-25 18:50:10 +0200

    Added open request for admin1.

--- webapp/src/main/resources/db/migrations/20181013_1043__add_vacation_request.sql
+++ webapp/src/main/resources/db/migrations/20181013_1043__add_vacation_request.sql
@@ -1,6 +1,6 @@
 create table vacation_request  (
   vacation_request_id bigserial primary key,
-  vacation_request_uuid varchar(80) not null,
+  vacation_request_uuid varchar(80) not null unique,
   user_id bigint not null,
   requested date not null,
   status varchar(80) not null,
@@ -14,3 +14,6 @@ create table vacation_request  (
 
 insert into vacation_request (vacation_request_uuid, user_id, status, requested, approved, approved_by_id, vac_From, vac_To, vacation_count, category) VALUES
 ('9e59d1dc-81bc-42f8-b1a3-1702bce3b824', 1, 'APPROVED','2021-06-02', '2021-06-03', 2, '2021-06-21', '2021-06-25', 5, 'PAID');
+
+insert into vacation_request (vacation_request_uuid, user_id, status, requested, approved, approved_by_id, vac_From, vac_To, vacation_count, category) VALUES
+('f1e89e8f-e65f-4fc5-9910-7e1b90b632ba', 1, 'REQUESTED','2021-06-23', null, null, '2021-06-28', '2021-06-29', 2, 'PAID');
